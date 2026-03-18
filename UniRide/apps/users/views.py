95782b38723470fdd232be05a9082cad8b26a3b7
mguzguz <126417118+mguzguz@users.noreply.github.com>
2026-03-17 20:38:22 -0500

    prueba correo

--- UniRide/apps/users/views.py
+++ UniRide/apps/users/views.py
@@ -317,7 +317,7 @@ class PreRegisterAdminView(APIView):
         # CORREO HTML CON LOGO
         # ===========================
 
-        confirmation_url = f""
+        confirmation_url = f"https://app.unirideweb.online/confirm-admin?code={code}&email={email}"
 
         logo_path = Path(settings.BASE_DIR) / "email_assets" / "logo-uniride2.png"
         
